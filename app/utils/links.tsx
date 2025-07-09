3bf545dc0e620fac106388ee8ce40ccdb15126e5
edeldev <edeldev25@gmail.com>
2025-07-09 15:20:00 -0600

    add link

--- app/utils/links.tsx
+++ app/utils/links.tsx
@@ -1,21 +1,31 @@
-import { IconBrandYoutube, IconWorld } from "@tabler/icons-react";
+import {
+  IconBrandTelegram,
+  IconBrandYoutube,
+  IconWorld,
+} from "@tabler/icons-react";
 import { TLink } from "./types";
 
 export const LINKS: TLink[] = [
   {
     id: 1,
+    title: "Únete a telegram",
+    link: "https://t.me/+do8ZTX4lcf5iNWNh",
+    icon: <IconBrandTelegram size={16} stroke={1} className="text-[#0088CC]" />,
+  },
+  {
+    id: 2,
     title: "Website",
     link: "https://edelhub.vercel.app",
     icon: <IconWorld size={16} stroke={1} className="text-secondary" />,
   },
   {
-    id: 2,
+    id: 3,
     title: "Portfolio Personal",
     link: "https://edeldev.vercel.app",
     icon: <IconWorld size={16} stroke={1} className="text-secondary" />,
   },
   {
-    id: 3,
+    id: 4,
     title: "Ultimo video youtube",
     link: "https://youtube.com/shorts/ol1_jOD3SHI?si=RIAJtgceFU366u3g",
     icon: <IconBrandYoutube size={16} stroke={1} className="text-red-700" />,
